82275aae5e8ce93b99043b9b71c408880b55f86e
Erik Rasmussen <erik.rasmussen@unmango.dev>
2026-03-28 15:36:19 -0500

    Linux build tag for cmd package (#32)
    * Linux build tag for cmd package
    
    * Unsupported platforms

--- pkg/cmd/cni.go
+++ pkg/cmd/cni.go
@@ -4,10 +4,6 @@ import (
 	"fmt"
 
 	"github.com/containernetworking/cni/pkg/skel"
-	"github.com/containernetworking/plugins/pkg/ns"
-	"github.com/unstoppablemango/wireguard-cni/pkg/config"
-	"github.com/unstoppablemango/wireguard-cni/pkg/network"
-	"github.com/unstoppablemango/wireguard-cni/pkg/wireguard"
 	"go.uber.org/zap"
 )
 
@@ -18,54 +14,9 @@ var (
 	ErrPrevResult  = fmt.Errorf("%s requires a prevResult", Name)
 )
 
-func withArgs(args *skel.CmdArgs) *zap.Logger {
+func logger(args *skel.CmdArgs) *zap.Logger {
 	return zap.L().With(
 		zap.String("interface", args.IfName),
 		zap.String("netns", args.Netns),
 	)
-}
-
-func Add(args *skel.CmdArgs) error {
-	withArgs(args).Info("cmdAdd")
-	conf, err := config.Parse(args.StdinData)
-	if err != nil {
-		return err
-	}
-	if conf.PrevResult != nil {
-		return ErrFirstPlugin
-	}
-
-	if err := ns.WithNetNSPath(args.Netns, func(ns.NetNS) error {
-		return wireguard.Add(network.New(args.IfName), conf)
-	}); err != nil {
-		return err
-	}
-
-	return conf.PrintResult(args)
-}
-
-func Del(args *skel.CmdArgs) error {
-	withArgs(args).Info("cmdDel")
-	if args.Netns == "" {
-		return nil
-	}
-
-	return ns.WithNetNSPath(args.Netns, func(ns.NetNS) error {
-		return network.New(args.IfName).Delete()
-	})
-}
-
-func Check(args *skel.CmdArgs) error {
-	withArgs(args).Info("cmdCheck")
-	conf, err := config.Parse(args.StdinData)
-	if err != nil {
-		return err
-	}
-	if conf.PrevResult == nil {
-		return ErrPrevResult
-	}
-
-	return ns.WithNetNSPath(args.Netns, func(ns.NetNS) error {
-		return wireguard.Check(network.New(args.IfName), conf)
-	})
 }
---
+++ pkg/cmd/cni_linux.go
@@ -0,0 +1,56 @@
+//go:build linux
+
+package cmd
+
+import (
+	"github.com/containernetworking/cni/pkg/skel"
+	"github.com/containernetworking/plugins/pkg/ns"
+	"github.com/unstoppablemango/wireguard-cni/pkg/config"
+	"github.com/unstoppablemango/wireguard-cni/pkg/network"
+	"github.com/unstoppablemango/wireguard-cni/pkg/wireguard"
+)
+
+func Add(args *skel.CmdArgs) error {
+	logger(args).Info("cmdAdd")
+	conf, err := config.Parse(args.StdinData)
+	if err != nil {
+		return err
+	}
+	if conf.PrevResult != nil {
+		return ErrFirstPlugin
+	}
+
+	if err := ns.WithNetNSPath(args.Netns, func(ns.NetNS) error {
+		return wireguard.Add(network.New(args.IfName), conf)
+	}); err != nil {
+		return err
+	}
+
+	return conf.PrintResult(args)
+}
+
+func Del(args *skel.CmdArgs) error {
+	logger(args).Info("cmdDel")
+	if args.Netns == "" {
+		return nil
+	}
+
+	return ns.WithNetNSPath(args.Netns, func(ns.NetNS) error {
+		return network.New(args.IfName).Delete()
+	})
+}
+
+func Check(args *skel.CmdArgs) error {
+	logger(args).Info("cmdCheck")
+	conf, err := config.Parse(args.StdinData)
+	if err != nil {
+		return err
+	}
+	if conf.PrevResult == nil {
+		return ErrPrevResult
+	}
+
+	return ns.WithNetNSPath(args.Netns, func(ns.NetNS) error {
+		return wireguard.Check(network.New(args.IfName), conf)
+	})
+}
