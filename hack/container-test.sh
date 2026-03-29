1d4701bebe06b0c2dcaaa80950ed88d92ece027c
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-03-29 12:38:26 -0500

    Comment

--- hack/container-test.sh
+++ hack/container-test.sh
@@ -3,6 +3,7 @@
 LABEL_FILTER="${LABEL_FILTER:-e2e && !k8s}"
 GOVERSION="$(go env GOVERSION | sed 's/go//')"
 GOMODCACHE="$(go env GOMODCACHE)"
+PODMAN="${PODMAN:-podman}"
 
 # CI runners don't always have this created yet
 mkdir -p "${GOMODCACHE}"
@@ -11,7 +12,7 @@ root="$(git rev-parse --show-toplevel)"
 
 set -x
 
-podman run --rm --privileged \
+$PODMAN run --rm --privileged \
     -v "$GOMODCACHE:/go/pkg/mod" \
     -v "$root:/src" \
     -w /src \
